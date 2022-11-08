6a5a30781c5428578b88fbfa6d74a8e667c50436
한상우 <sktkddn777@naver.com>
2022-11-08 20:33:07 +0900

    Feat: JDBC 조회 수정 삭제
    추가로 테스트 코드 작성

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -25,6 +26,75 @@ public class MemberRepositoryV0 {
             pstmt.setInt(2, member.getMoney());
             pstmt.executeUpdate();
             return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }
+        finally {
+            close(con, pstmt, null); // 외부 TCP IP 를 통한 연결 리소스를 사용하고 있으므로 close 해야함
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }
+        finally {
+            close(con, pstmt, rs); // 외부 TCP IP 를 통한 연결 리소스를 사용하고 있으므로 close 해야함
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("update result = " + resultSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }
+        finally {
+            close(con, pstmt, null); // 외부 TCP IP 를 통한 연결 리소스를 사용하고 있으므로 close 해야함
+        }
+    }
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("update result = " + resultSize);
         } catch (SQLException e) {
             log.error("db error", e);
             throw e;
