586e41bbe868335de957726eeee3c58159962efc
한상우 <sktkddn777@naver.com>
2022-11-16 19:57:38 +0900

    Add: Checked Exception, UnChecked Exception

---
+++ jdbc/src/test/java/hello/jdbc/exception/basic/CheckedTest.java
@@ -0,0 +1,64 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class CheckedTest {
+
+    @Test
+    void checked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void checked_throw() {
+        Service service = new Service();
+        Assertions.assertThatThrownBy(() -> service.callThrow())
+                .isInstanceOf(MyCheckedException.class);
+
+    }
+
+    /**
+     * Exception 을 상속 받은 예외는 체크 예외가 된다.
+     */
+    static class MyCheckedException extends Exception {
+        public MyCheckedException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Checked 예외는 예외를 잡아서 처리하거나 던진다
+     */
+    static class Service {
+        Repository repository = new Repository();
+
+        /**
+         * 예외를 잡아서 처리
+         */
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyCheckedException e) {
+                log.info("예외 처리 {}", e.getMessage());
+            }
+        }
+
+        /**
+         * 체크 예외를 밖으로 던지는 코드
+         * 체크 예외는 throws 예외를 메서드 필드에사 던져야 한다
+         */
+        public void callThrow() throws MyCheckedException {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() throws MyCheckedException {
+            throw new MyCheckedException("ex");
+        }
+    }
+}
---
+++ jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
@@ -0,0 +1,65 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class UncheckedTest {
+
+    @Test
+    void unchecked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw() {
+        Service service = new Service();
+        Assertions.assertThatThrownBy(() -> service.callThrow())
+                .isInstanceOf(MyUnCheckedException.class);
+
+    }
+
+    /**
+     * RuntimeException 을 상속받은 예외는 언체크 예외
+     */
+    static class MyUnCheckedException extends RuntimeException {
+        public MyUnCheckedException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * UnChecked 예외는 예외를 잡거나 던지지 않아도 된다.
+     * 예외를 잡지 않으면 자동으로 밖으로 던짐
+     */
+    static class Service {
+        Repository repository = new Repository();
+
+        /**
+         * 필요한 경우 예외를 잡아서 처리
+         */
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyUnCheckedException e) {
+                log.info("error = {}", e.getMessage());
+            }
+        }
+
+        /**
+         * 예외를 잡지 않아도 된다
+         */
+        public void callThrow() {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            throw new MyUnCheckedException("ex");
+        }
+    }
+
+}
